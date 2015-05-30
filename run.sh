e91122c48c3da97dc1ca7497d9fc414938fc7888
altoidnerd <altoidnerd@altoidnerds-iMac.local>
2015-05-30 08:50:58 -0400

    commented run.sh and made /dev/urandom the default source

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-dd if=/dev/random of=./randfile bs=64 count=10 && hexdump -C randfile > infile; ./plothex.py infile; rm randfile infile
+
+# On linux, using /dev/random and /dev/urandom will produce different results. 
+# Linux users will have to reduce the block count or wait for more
+# entropy is /dev/random is used.
+
+dd if=/dev/urandom of=./randfile bs=64 count=10 && hexdump -C randfile > infile; ./plothex.py infile; rm randfile infile
 
 
