c42efd072a599eab66b0310e5a437579e67bab35
altoidnerd <altoidnerd@altoidnerds-iMac.local>
2015-05-30 08:51:48 -0400

    fixed typo

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 
 # On linux, using /dev/random and /dev/urandom will produce different results. 
 # Linux users will have to reduce the block count or wait for more
-# entropy is /dev/random is used.
+# entropy if /dev/random is used.
 
 dd if=/dev/urandom of=./randfile bs=64 count=10 && hexdump -C randfile > infile; ./plothex.py infile; rm randfile infile
 
