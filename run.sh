1d7328185098b07aa25ef1500ec10a49d8d04ba1
altoidnerd <altoidnerd@altoidnerds-iMac.local>
2015-05-30 05:48:40 -0400

    fixed shebang run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-dd if=/dev/random of=./randfile bs=1024 count=2 && hexdump randfile > infile; ./plothex.py infile
+dd if=/dev/random of=./randfile bs=64 count=100 && hexdump randfile > infile; ./plothex.py infile
 
 
